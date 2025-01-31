8de84607892305776c54d0b5a10586f70b5d251e
yashaswinigande <gandeyashaswini431@gmail.com>
2025-01-31 20:48:52 +0530

    Add files via upload

---
+++ class Default.java
@@ -0,0 +1,29 @@
+class Default{
+        static byte defaultByte;
+        static char defaultChar;
+        static int defaultInt;
+        static float defaultFloat;
+        static double defaultDouble;
+        static short defaultShort;
+        static long defaultLong;
+        static boolean defaultBoolean;
+        public static void main(String[]args){
+        System.out.println("byte:"+defaultByte);
+        System.out.println("char:"+defaultChar);
+        System.out.println("int:"+defaultInt);
+        System.out.println("float:"+defaultFloat);
+        System.out.println("double:"+defaultDouble);
+        System.out.println("short:"+defaultShort);
+        System.out.println("long:"+defaultLong);
+        System.out.println("boolean:"+defaultBoolean);
+    }
+}
+output:
+byte:0
+char:
+int:0
+float:0.0
+double:0.0
+short:0
+long:0
+boolean: false
